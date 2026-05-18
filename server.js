0f5100b64fcebdf71ff40048cd7cf49aa5ecd0da
Abhishek idap <abdxpro17@gmail.com>
2026-05-18 19:22:41 +0530

    Fix render route

--- server.js
+++ server.js
@@ -118,8 +118,12 @@ async function buildExcel(data) {
 
 // ── Middleware ─────────────────────────────────────────────────────
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("*", (req,res)=>{
+   res.sendFile(path.join(__dirname,"public","index.html"));
+});
 // ── API Routes ─────────────────────────────────────────────────────
 
 // Get full state
